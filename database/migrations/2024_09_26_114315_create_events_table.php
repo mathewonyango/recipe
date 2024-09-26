0e9e743181824576c0b2da8bb1914131ec24f5f4
Mathews Mc'Agumbah <mathewsagumbah@gmail.com>
2024-09-26 21:14:24 +0300

    adding env

--- database/migrations/2024_09_26_114315_create_events_table.php
+++ database/migrations/2024_09_26_114315_create_events_table.php
@@ -9,13 +9,15 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
+        Schema::dropIfExists('events'); // Drop the table if it exists
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
