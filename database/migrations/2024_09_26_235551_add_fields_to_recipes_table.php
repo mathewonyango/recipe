9bcaa8ab94b48272425357d586a6c459f1ab72c5
Mathews Mc'Agumbah <mathewsagumbah@gmail.com>
2024-09-27 02:56:24 +0300

    adding env

---
+++ database/migrations/2024_09_26_235551_add_fields_to_recipes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('recipes', function (Blueprint $table) {
+            $table->string('image')->nullable(false)->change();
+            $table->string('tags')->nullable(false)->change();
+            $table->enum('difficulty_level', ['easy', 'medium', 'hard'])->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('recipes', function (Blueprint $table) {
+            $table->string('image')->nullable()->change();
+            $table->string('tags')->nullable()->change();
+            $table->enum('difficulty_level', ['easy', 'medium', 'hard'])->nullable()->change();
+        });
+    }
+};
