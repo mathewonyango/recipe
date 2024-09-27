5283d4d9bf131844a8b903a500a19a5500de3922
Mathews Mc'Agumbah <mathewsagumbah@gmail.com>
2024-09-27 12:06:01 +0300

    reset password

--- database/migrations/2024_09_26_235551_add_fields_to_recipes_table.php
+++ database/migrations/2024_09_26_235551_add_fields_to_recipes_table.php
@@ -9,14 +9,14 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
-    {
-        Schema::table('recipes', function (Blueprint $table) {
-            $table->string('image')->nullable(false)->change();
-            $table->string('tags')->nullable(false)->change();
-            $table->enum('difficulty_level', ['easy', 'medium', 'hard'])->nullable(false)->change();
-        });
-    }
+    // public function up()
+    // {
+    //     Schema::table('recipes', function (Blueprint $table) {
+    //         $table->string('image')->nullable(false)->change();
+    //         $table->string('tags')->nullable(false)->change();
+    //         $table->enum('difficulty_level', ['easy', 'medium', 'hard'])->nullable(false)->change();
+    //     });
+    // }
 
     /**
      * Reverse the migrations.
