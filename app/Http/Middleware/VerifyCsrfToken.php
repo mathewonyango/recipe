fd778d522d4ef955440b4ccd565ae954a87a3ed5
Mathews Mc'Agumbah <mathewsagumbah@gmail.com>
2024-09-29 12:42:09 +0300

    chef registeration

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,21 +14,9 @@ class VerifyCsrfToken
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
 
-     protected $except = [
-        'api/*',      // Exclude all API routes
-    ];
-
     public function handle(Request $request, Closure $next): Response
     {
-        // Allow all requests without CSRF token for the specified routes
-        foreach ($this->except as $except) {
-            if ($request->is($except)) {
-                return $next($request); // Skip CSRF verification for excluded routes
-            }
-        }
-
-        // For other requests, you can choose to not perform CSRF checks at all
+        // Allow all requests to proceed without CSRF verification
         return $next($request);
     }
-
 }
