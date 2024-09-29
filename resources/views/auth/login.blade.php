dcb238587f40ad44c04f1a8588df377f8ce5719f
Mathews Mc'Agumbah <mathewsagumbah@gmail.com>
2024-09-29 22:25:50 +0300

    chef registeration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                             <div class="col-md-6 p-5">
                                 <div class="mx-auto mb-5">
                                     <a href="/">
-                                        <img src="assets/images/logo.png" alt="" height="24" />
+                                        <img src="assets/images/recipe.png" alt="" height="24" />
                                         <h3 class="d-inline align-middle ml-1 text-logo">Login</h3>
                                     </a>
                                 </div>
