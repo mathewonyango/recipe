ebb241764bcee1a582450d5fc7966eee80cb3d17
Mathews Mc'Agumbah <mathewsagumbah@gmail.com>
2024-09-29 13:48:01 +0300

    chef registeration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
 
 
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(VerifyCsrfToken::class);
+        // $middleware->append(VerifyCsrfToken::class);
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
 
 
         //
