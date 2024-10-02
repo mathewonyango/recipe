5645df11755d5bc55144ba6295520358ff22f745
Mathews Mc'Agumbah <mathewsagumbah@gmail.com>
2024-10-02 16:34:31 +0300

    fetching recipe id

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use APP\Models\Payment;
+use App\Models\Payment;
 
 
 class PaymentController extends Controller
